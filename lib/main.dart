4d93949e641b9a157e5ee28313b7a4b1a60dba69
smedinam <smedina@kunder.cl>
2022-07-04 17:02:34 -0400

    test diff coverage

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Test app",
+      title: "Flutter Test appp",
       theme: ThemeData(
         // This is the theme of your application.
         //
